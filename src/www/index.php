6c5a13ab11aa9bfa7f45952040a67c619ea1738e
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-05-13 09:22:38 +0200

    Implement function to get cod2 server addresses from env

--- src/www/index.php
+++ src/www/index.php
@@ -1,1 +1,19 @@
-<? echo "Hello world"; ?>
+<?
+function getAllCod2Servers() {
+    $servers = array();
+    $i = 1;
+
+    while ($address = getenv('COD2_SERVER' . $i . '_ADDRESS')) {
+        $port = getenv('COD2_SERVER' . $i . '_PORT');
+
+        $server = new stdClass();
+        $server->address = $address;
+        $server->port = $port;
+
+        $servers[] = $server;
+        $i++;
+    }
+
+    return $servers;
+}
+?>
